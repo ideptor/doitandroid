8d8c6c390f1fc2446e232969d102152e60d2c9e5
Suhyun Kim <ideptor@gmail.com>
2018-06-22 01:33:35 +0900

    day02-05

--- Hello/app/src/main/java/org/androidtown/hello/MainActivity.java
+++ Hello/app/src/main/java/org/androidtown/hello/MainActivity.java
@@ -1,7 +1,11 @@
 package org.androidtown.hello;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View view) {
+        Toast.makeText(getApplicationContext(), "시작 버튼이 눌러졌습니다.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButton2Clicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(intent);
+    }
+
+
+    public void onButton3Clicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(intent);
     }
 }
