6a64416bdf51df811131be4fbb1e937c86d3b3c8
Suhyun Kim <ideptor@gmail.com>
2018-06-26 15:02:34 +0900

    day17 permission

--- MySMSReceiver/app/src/main/java/org/androidtown/smsreceiver/MainActivity.java
+++ MySMSReceiver/app/src/main/java/org/androidtown/smsreceiver/MainActivity.java
@@ -1,7 +1,14 @@
 package org.androidtown.smsreceiver;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "SMS Receive permission is allowed", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "SMS Receive permission is not allowed", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECEIVE_SMS)) {
+                Toast.makeText(this, "Need to explain SMS Permission", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "SMS Permission obtained", Toast.LENGTH_LONG).show();
+                    } else if(grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "SMS Permission denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+        }
     }
 }
